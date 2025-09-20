70222107592855add3e14481c67a2a22867feb07
AndresMorquecho <morquechoisrael@hotmail.com>
2025-09-19 20:51:45 -0500

    se agrega variables de entorno para test | env.test

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require("dotenv").config();
 
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const jwtSecret = process.env.JWT_SECRET;
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.get("/",(req, res)=>{
-    res.send("Api backend funcionando");
-})
+console.log("Clave secreta:", jwtSecret);
+console.log("port:", port);
+
+app.get("/", (req, res) => {
+  res.send("Api backend funcionando");
+});
